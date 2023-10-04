7d624b9db6d7bcb37f3261eecb8916aed18f0c88
Marco <mavelandiag74@gmail.com>
2023-10-03 22:00:32 -0500

    primer avance pagina investigacion

---
+++ src/componentes/investigacion/CardInvestigacion.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+
+export const CardInvestigacion=({title, imgUrl, imgback, categoria, depto, lider, email})=> {
+    return(
+        <>
+         <div className="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
+            <div className="service-item">
+              <div className="img">
+                <img src={imgback} className="img-fluid" alt=""/>
+              </div>
+              <div className="details position-relative">
+                <div className="icon">
+                <img src={imgUrl} className="img-fluid" alt=""/>
+                </div>
+                  <h3>{title}</h3>
+                <p><strong>{categoria}</strong></p>
+                <p className="descripcion">{depto}</p>
+                <p>{lider}</p>
+                <p>{email}</p>
+              </div>
+            </div>
+          </div>
+        </>
+
+    )
+} 
--- src/pages/Investigacion.jsx
+++ src/pages/Investigacion.jsx
@@ -1,8 +1,81 @@
 import React from "react";
-import '../css/investigacion.css'
-import invest1Img from '../img/investigacion/logo_gcena.svg'
-import prueba from '../img/facultades/admon1.jpeg'
+import '../css/investigacion.css';
+import prueba from '../img/investigacion/inves1.jpg';
+import logoImg from '../img/investigacion/logo_gcena.svg'
+import {CardInvestigacion} from '../componentes/investigacion/CardInvestigacion'
+
+
 const Investigación = () => {
+
+  const Cardinvestigacion = [
+    {
+      title: "Grupo de investigación en Seguridad Industrial, Salud Ocupacional y Medio Ambiente",
+      imgUrl: logoImg,
+      imgback: prueba,
+      categoria: "COL0107136- Categoría C",
+      depto: "Facultad de ingeniería –Ingeniería en Seguridad Industrial e Higiene Ocupacional y Técnico Profesional en Seguridad y Salud en el trabajo",
+      lider: "Líder(es): Diana Tamayo-Camilo Torres",
+      email: "dirinvestigaciones@unihorizonte.edu.co",
+    },
+    {
+      title: "Grupo de Investigación en Enseñanza de las Ciencias Exactas y Naturales Aplicadas",
+      imgUrl: logoImg,
+      imgback: prueba,
+      categoria: "COL0199192-Categoría C",
+      depto: "Facultad de ingeniería",
+      lider: "Líder(es): Luisa García-Diana Tamayo",
+      email: "garcia_luisa@unihorizonte.edu.co",
+    },
+    {
+      title: "Grupo de Investigación DIONE",
+      imgUrl: logoImg,
+      imgback: prueba,
+      categoria: "COL0202304",
+      depto: "Facultad de Ciencias Administrativas",
+      lider: "Líder: Efrén Antonio Calderón",
+      email: "efren_calderon20212@unihorizonte.edu.co",
+    },
+    {
+      title: "Grupo de Investigación Horizonte Cultural Gastronómico",
+      imgUrl: logoImg,
+      imgback: prueba,
+      categoria: "COL0231754",
+      depto: "Facultad de Gastronomía",
+      lider: "Líder: Edy Mayerly González",
+      email: "",
+    },
+    {
+      title: "Grupo de Investigación Vértice",
+      imgUrl: logoImg,
+      imgback: prueba,
+      categoria: "COL0232223",
+      depto: "Facultad de Comunicación y Marketing Digital-Diseño gráfico, comunicación y mercadeo",
+      lider: "Líder: Martha Cristina Ramírez",
+      email: "comunicacionsocial@unihorizonte.edu.co",
+    },
+    {
+      title: "Grupo de Investigación TECH-INNOVA",
+      imgUrl: logoImg,
+      imgback: prueba,
+      categoria: "COL0229658",
+      depto: "Facultad de ingeniería – ingeniería de Software",
+      lider: "Líder: Diana Patricia Camargo",
+      email: "",
+    },
+    {
+      title: "Grupo de Investigación NANSHE",
+      imgUrl: logoImg,
+      imgback: prueba,
+      categoria: "COL0233982",
+      depto: "Facultad de Ciencias Jurídicas",
+      lider: "Líder: Jayme Baez",
+      email: "derecho@unihorizonte.edu.co",
+    },
+  ];
+  
+
+
+
     return(
         <>
 
@@ -12,32 +85,29 @@ const Investigación = () => {
         <h2>Investigación</h2>
 
         <p className="descripcion text-justify">
-          En la Fundación Universitaria Horizonte la investigación se constituye como fuente de saber, generadora y soporte del ejercicio docente, que como propósito principal tiene la producción y validación de nuevo conocimiento para impulsar el desarrollo académico, científico, tecnológico, cultural y social del país. Para lograr este fin, el Proyecto Educativo Institucional (PEI) de la fundación Universitaria Horizonte propone desde la óptica de la investigación, “una universidad comprometida con la cultura investigativa, el pensamiento científico y tecnológico” a través de un modelo pedagógico de desarrollo humano, que está orientado a “visibilizar el conocimiento y los actores que se encuentran tras las huellas de este”
+          En la Fundación Universitaria Horizonte la investigación se constituye como fuente de saber, generadora y soporte del ejercicio docente, que como propósito principal tiene la producción y validación de nuevo conocimiento para impulsar el desarrollo académico, científico, tecnológico, cultural y social del país. Para lograr este fin, el Proyecto Educativo Institucional (PEI) de la fundación Universitaria Horizonte propone desde la óptica de la investigación, “una universidad comprometida con la cultura investigativa, el pensamiento científico y tecnológico” a través de un modelo pedagógico de desarrollo humano, que está orientado a “visibilizar el conocimiento y los actores que se encuentran tras las huellas de este.”
           </p>
         </div>
 
 
         <div className="row gy-5">
 
-          <div className="col-xl-4 col-md-6" data-aos="zoom-in" data-aos-delay="200">
-            <div className="service-item">
-              <div className="img">
-                <img src={prueba} className="img-fluid" alt=""/>
-              </div>
-              <div className="details position-relative">
-                <div className="icon">
-                <img src={invest1Img} className="img-fluid" alt=""/>
-                </div>
-                <a href="#" className="stretched-link">
-                  <h3>Grupo de investigación en Seguridad Industrial, Salud Ocupacional y Medio Ambiente</h3>
-                </a>
-                <p>COL0107136- Categoría C</p>
-                <p>Facultad de ingeniería –Ingeniería en Seguridad Industrial e Higiene Ocupacional y Técnico Profesional en Seguridad y Salud en el trabajo</p>
-                <p>Líder(es): Diana Tamayo-Camilo Torres</p>
-                <p>dirinvestigaciones@unihorizonte.edu.co</p>
-              </div>
-            </div>
-          </div>
+        <div className="col-lg-12 col-md-12 text-center align-items-center">
+        <h2 className="title-grupos">Nuestros Grupos de investigación</h2>
+        </div>
+
+        {
+                          Cardinvestigacion.map((container, index) => {
+                            return (
+                              <CardInvestigacion
+                                key={index}
+                                {...container}
+                                />
+                            )
+                          })
+                        }
+
+         
 
           
         </div>
