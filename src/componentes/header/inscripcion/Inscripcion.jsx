a81083f973f37e303666b0a6a5d47f0a6e478b8a
Marco <mavelandiag74@gmail.com>
2023-11-08 21:27:37 -0500

    se agregan textos reales al componente inscripción del home y se quita por ahora el call to actiono

--- src/componentes/header/inscripcion/Inscripcion.jsx
+++ src/componentes/header/inscripcion/Inscripcion.jsx
@@ -20,15 +20,17 @@ const Inscripcion = () =>{
         <div className="col-lg-7 contenido">
           <h2>Queremos conocerte</h2>
           <div className="our-story">
-            <p>¡Prepárate para un emocionante viaje educativo en la U de la felicidad, el emprendimiento, la tecnología y la innovación! Completa el siguiente formulario de preinscripción, y pronto te contactaremos.</p>
-            <ul>
-              <li><i className="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea commo</span></li>
-              <li><i className="bi bi-check-circle"></i> <span>Duis aute irure dolor in reprehenderit in</span></li>
-              <li><i className="bi bi-check-circle"></i> <span>Ullamco laboris nisi ut aliquip ex ea</span></li>
-            </ul>
-            <p>Vitae autem velit excepturi fugit. Animi ad non. Eligendi et non nesciunt suscipit repellendus porro in quo eveniet. Molestias in maxime doloremque.</p>
+            <p>¡Convierte tus Sueños en Realidad Ahora!</p>
+            <p>Ven a la U de la Felicidad, el Bilingüismo, la innovación, la tecnología y el emprendimiento. </p>
+            <p>Entra en el mundo de infinitas posibilidades. ¿Alguna vez has deseado cambiar tu vida, alcanzar tus metas más ambiciosas y vivir el estilo de vida que siempre has soñado? ¡Ahora es tu momento! Pero para hacerlo posible, necesitamos tu ayuda. </p>
+            <p>Nosotros estamos aquí para ayudarte a transformar tus sueños en realidad, estamos para ti, para que tus pasiones sean lo que mueve tú ser. Necesitamos tu ayuda, déjanos tus datos para que podamos ofrecerte las herramientas y recursos adecuados. Con tu información, podemos personalizar nuestra orientación y proporcionarte el apoyo que necesitas. </p>
 
-            <p>Vitae autem velit excepturi fugit. Animi ad non. Eligendi et non nesciunt suscipit repellendus porro in quo eveniet. Molestias in maxime doloremque.</p>
+            <p>¿Qué obtendrás al dejarnos tus datos? </p>
+            <ul>
+              <li><i className="bi bi-check-circle"></i> <span>Asesoramiento Personalizado: Nuestro equipo estará a tu disposición para guiarte en cada paso del camino hacia el éxito. </span></li>
+              <li><i className="bi bi-check-circle"></i> <span>Comunidad de Apoyo: Conectarás con una comunidad de personas apasionadas y motivadas como tú, dispuestas a compartir experiencias y brindarte apoyo incondicional.</span></li>
+              
+            </ul>
           </div>
         </div>
 
